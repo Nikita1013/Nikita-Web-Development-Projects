512fbcff7dffa58e9db5269ec4b2888be97101d4
Nikita1013 <shindenikita646@gmail.com>
2024-06-29 15:54:23 +0530

    commit-ejs

--- EJS/4.3 EJS Partials/index.js
+++ EJS/4.3 EJS Partials/index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Step 2: Make sure that static files are linked to and the CSS shows up.
 app.use(express.static("public"));
 
 
@@ -11,6 +12,7 @@ app.get("/",(req, res) => {
   res.render("index.ejs");
 });
 
+// Step 3: Add the routes to handle the render of the about and contact pages.
 app.get("/about",(req, res) => {
   res.render("about.ejs");
 });
@@ -18,8 +20,7 @@ app.get("/about",(req, res) => {
 app.get("/contact",(req, res) => {
   res.render("contact.ejs");
 });
-// Step 2: Make sure that static files are linked to and the CSS shows up.
-// Step 3: Add the routes to handle the render of the about and contact pages.
+
   // Hint: Check the nav bar in the header.ejs to see the button hrefs
 // Step 4: Add the partials to the about and contact pages to show the header and footer on those pages.
 app.listen(port, () => {
